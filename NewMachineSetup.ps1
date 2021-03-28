e2caa71e34ca83b40bfa02123484d27e967489bd
Antony Bailey <hi@antonybailey.net>
2021-03-28 19:04:48 +0100

    Add azure-documentdb-data-migration-tool

--- NewMachineSetup.ps1
+++ NewMachineSetup.ps1
@@ -86,6 +86,7 @@ installWithChoco "cmake"
 # Database management
 installWithChoco "pgadmin3"
 installWithChoco "ssms"
+installWithChoco "azure-documentdb-data-migration-tool"
 # Network tools
 installWithChoco "openssh"
 installWithChoco "putty.install"
