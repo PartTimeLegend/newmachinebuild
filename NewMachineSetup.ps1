f2fb3e086c3ddded178124a485da643367ddc6cd
Antony Bailey <hi@antonybailey.net>
2021-03-31 14:54:41 +0100

    Add wireshark

--- NewMachineSetup.ps1
+++ NewMachineSetup.ps1
@@ -106,6 +106,7 @@ installWithChoco "filezilla"
 installWithChoco "mremoteng"
 installWithChoco "teamviewer"
 installWithChoco "curl"
+installWithChoco "wireshark"
 # Cloud tools
 installWithChoco "awscli"
 installWithChoco "azure-cli"
