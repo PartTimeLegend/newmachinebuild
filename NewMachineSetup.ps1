9771bfd3680367e4a5be2eb5b532fc4323695ef1
Antony Bailey <hi@antonybailey.net>
2021-04-06 17:39:31 +0100

    Add Office 365

--- NewMachineSetup.ps1
+++ NewMachineSetup.ps1
@@ -132,6 +132,12 @@ installWithChoco "vlc"
 installWithChoco "jabra-direct"
 # Features
 installWithChoco "wsl2"
+# Business tools
+switch ($windowsCaption)
+{
+  {$_.Contains("Home")} { installWithChoco "office365homepremium" }
+  Default { installWithChoco "office365business"}
+}
 # List Packages
 choco list --local-only
 # Enable Windows Features
