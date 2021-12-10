782eab3fc27ec1642185d5c35a85b43bd010ca0e
Antony Bailey <hi@antonybailey.net>
2021-12-10 13:52:30 +0000

    Create NewMachineSetup.sh

---
+++ NewMachineSetup.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
+brew install git-lfs
+# File comparison
+brew install meld
+# Languages
+brew install ruby
+brew install go
+brew install python
+brew install dotnet
+brew install dotnet-sdk
+brew install node
+# IDEs
+brew install visual-studio-code
+brew install postman
+brew install visual-studio
+# Text Editors
+brew install 010-editor
+# Financial
+brew install electrum
+# Browsers
+brew install google-chrome
+brew install firefox
+# Infrastructure
+brew install terraform
+brew install packer
+brew install tflint
+# Utilities
+brew install p7zip
+brew install powershell
+brew install cmake
+brew install burp-suite
+brew install autopsy
+brew install balenaetcher
+brew install yarn
+# Database management
+brew install pgadmin4
+brew install azure-data-studio
+brew install nosql-workbench
+brew install sqltoolbelt
+# Network tools
+brew install openssh
+brew install openssl
+brew install slack
+brew install teamviewer
+brew install curl
+brew install wireshark
+brew install nmap
+brew install wireguard-go
+# Cloud tools
+brew install awscli
+brew install azure-cli
+
+# Containers
+brew install --cask docker
+brew install helm
+brew install kubernetes-cli
+brew install minikube
+# Video Calls
+brew install zoom
+brew install microsoft-teams
+# Go get
+go get -u -u github.com/jrhouston/tfk8s
+# Ruby gems - I will put these in a Gemfile
+gem install terraforming
