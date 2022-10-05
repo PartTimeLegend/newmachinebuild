13d4ce88c5927df34002540aa3bc3605dd179081
Antony Bailey <hi@antonybailey.net>
2022-10-05 16:08:31 +0100

    brews.txt (#144)

--- NewMachineSetup.sh
+++ NewMachineSetup.sh
@@ -1,53 +1,10 @@
 #!/usr/bin/env bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
-packages=("git"
-  "git-lfs"
-  "meld"
-  "ruby"
-  "go"
-  "python"
-  "dotnet"
-  "dotnet-sdk"
-  "node"
-  "visual-studio-code"
-  "postman"
-  "visual-studio"
-  "010-editor"
-  "electrum"
-  "google-chrome"
-  "firefox"
-  "terraform"
-  "packer"
-  "tflint"
-  "p7zip"
-  "powershell"
-  "cmake"
-  "burp-suite"
-  "autopsy"
-  "balenaetcher"
-  "yarn"
-  "pgadmin4"
-  "azure-data-studio"
-  "nosql-workbench"
-  "openssh"
-  "openssl"
-  "slack"
-  "teamviewer"
-  "curl"
-  "wireshark"
-  "nmap"
-  "wireguard-go"
-  "awscli"
-  "azure-cli"
-  "docker"
-  "helm"
-  "kubernetes-cli"
-  "minikube"
-  "zoom"
-  "microsoft-teams"
-  "k9s"
-  "act")
+packages=()
+while IFS= read -r line; do
+   packages+=("$line")
+done <brews.txt
 
 install () {
   brew install $1
