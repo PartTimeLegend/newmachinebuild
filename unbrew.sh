8f7fa368bc6f2abdb40825fd7050f8387a296562
Antony Bailey <antonybailey@Antonys-MacBook-Air.local>
2023-06-30 10:19:09 +0100

    Add Unbrew

--- unbrew.sh
+++ unbrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 while [[ `brew list | wc -l` -ne 0 ]]; do
-    for package in `brew list`; do
-        brew uninstall --force --ignore-dependencies $package
-    done
+  for package in `brew list`; do
+    brew uninstall --force --ignore-dependencies $package
+  done
 done
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
